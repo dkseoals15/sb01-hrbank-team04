740974fd35c9ae272ef137f0019e6535a45dc4df
hyojkim2 <skql2548@gmail.com>
2025-03-17 19:35:02 +0900

    :sparkles: employee 엔티티 추가

---
+++ src/main/java/com/codeit/sb01hrbankteam04/domain/employee/entity/Employee.java
@@ -0,0 +1,48 @@
+package com.codeit.sb01hrbankteam04.domain.employee.entity;
+
+import com.codeit.sb01hrbankteam04.domain.department.Department;
+import com.codeit.sb01hrbankteam04.domain.file.File;
+import com.codeit.sb01hrbankteam04.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.Instant;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+@Entity
+@Table(name = "employee")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Employee extends BaseEntity {
+
+  @Column(nullable = false, length = 20)
+  private String status;
+
+  @Column(nullable = false, length = 100)
+  private String name;
+
+  @Column(nullable = false, unique = true, length = 70)
+  private String email;
+
+  @Column(nullable = false, unique = true, length = 30)
+  private String code;
+
+  @ManyToOne
+  @JoinColumn(name = "department_id", foreignKey = @ForeignKey(name = "fk_department"))
+  @OnDelete(action= OnDeleteAction.SET_NULL)
+  private Department department;
+
+  @Column(nullable = false, length = 50)
+  private String position;
+
+  @Column(nullable = false)
+  private Instant joinedAt;
+
+  @ManyToOne
+  @JoinColumn(name = "profile_id", foreignKey = @ForeignKey(name = "fk_profile"))
+  @OnDelete(action= OnDeleteAction.SET_NULL)
+  private File profile;
+}
