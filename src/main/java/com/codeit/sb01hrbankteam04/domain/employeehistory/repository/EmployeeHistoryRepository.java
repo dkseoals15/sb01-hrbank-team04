9bfd964e8a8ac69b0b3ef4641719101d90e2151d
junwoo <junwoo981007@naver.com>
2025-03-18 00:36:07 +0900

    :fire: Delete Repository - Employee History 레포지토리를 Employee 레포지토리에 합칠 계획

--- src/main/java/com/codeit/sb01hrbankteam04/domain/employeehistory/repository/EmployeeHistoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.codeit.sb01hrbankteam04.domain.employeehistory.repository;
-
-import org.springframework.data.repository.history.RevisionRepository;
-
-//public interface EmployeeHistoryRepository extends RevisionRepository<Employee, Long, Long> {
-//
-//}
