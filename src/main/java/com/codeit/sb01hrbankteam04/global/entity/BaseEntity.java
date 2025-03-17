95ac9635b8a8da0f3e36faa23b934140da66d303
silvarge <sep.non2@gmail.com>
2025-03-17 10:04:09 +0900

    :wrench: chore: 공통 응답 객체 수정 및 BaseEntity 추가
    - 공통 응답 객체 수정
    - BaseEntity, BaseUpdatableEntity 추가
    - CheckStyle에 따라 global 패키지 내 코드들에 주석 추가
    
    resolves: #3

---
+++ src/main/java/com/codeit/sb01hrbankteam04/global/entity/BaseEntity.java
@@ -0,0 +1,29 @@
+package com.codeit.sb01hrbankteam04.global.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import java.time.Instant;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+
+/**
+ * 엔티티 작성 시 공통적으로 들어가는 값들에 대해 정의
+ * <p>
+ * ID 생성 및 생성 일시를 매핑해 줌
+ */
+@MappedSuperclass
+@Getter
+public abstract class BaseEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @CreatedDate
+  @Column(name = "created_at", nullable = false, updatable = false, insertable = false)
+  private Instant createdAt;
+
+}
