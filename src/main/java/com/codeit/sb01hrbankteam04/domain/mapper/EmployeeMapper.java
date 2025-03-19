24a36c34fe7152886f88d6fa5ead40b20fb33ec0
hyojkim2 <skql2548@gmail.com>
2025-03-19 18:10:16 +0900

    :sparkles: 직원 API 구현: Mapper, Utils

---
+++ src/main/java/com/codeit/sb01hrbankteam04/domain/mapper/EmployeeMapper.java
@@ -0,0 +1,29 @@
+package com.codeit.sb01hrbankteam04.domain.mapper;
+
+import com.codeit.sb01hrbankteam04.domain.employee.dto.EmployeeResponse;
+import com.codeit.sb01hrbankteam04.domain.employee.entity.Employee;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class EmployeeMapper  {
+
+  public EmployeeResponse toDto(Employee employee) {
+
+    return new EmployeeResponse(
+        employee.getId(),
+        employee.getName(),
+        employee.getEmail(),
+        employee.getCode(),
+        employee.getDepartment().getId(),
+        employee.getDepartment().getName(),
+        employee.getPosition(),
+        employee.getJoinedAt(),
+        employee.getStatus().toString(),
+        employee.getProfile() != null ? employee.getProfile().getId() : null // profile이 null이면 null 반환
+    );
+  }
+
+
+}
