e51a2fdb0dfa58f36da21aca3fafc2dc373d329e
hyojkim2 <skql2548@gmail.com>
2025-03-19 18:37:26 +0900

    :sparkles: 직원 API 구현: DTO

---
+++ src/main/java/com/codeit/sb01hrbankteam04/domain/employee/dto/EmployeePageResponse.java
@@ -0,0 +1,29 @@
+package com.codeit.sb01hrbankteam04.domain.employee.dto;
+
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class EmployeePageResponse {
+  List<EmployeeResponse> content;
+  String nextCursor;
+  Long nextIdAfter;
+  int size;
+  long totalElements;
+  boolean hasNext;
+
+  public static EmployeePageResponse from(
+      List<EmployeeResponse> employeeResponses,
+      String nextCursor,
+      Long lastId,
+      int size,
+      Long totalElements,
+      boolean hasNext) {
+    return new EmployeePageResponse(employeeResponses, nextCursor, lastId, size, totalElements, hasNext);
+
+  }
+}
